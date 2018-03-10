62eaec79afa8dfeaa21e2ff0064624d7ab0a241c
Victor Pinheiro <vpinheiro.aguiar@gmail.com>
2018-03-10 11:35:41 -0300

    Another update
    - Toad finished;
    - Some castle blocks are implemented.

---
+++ src/SuperMarioBros/Assets/Scripts/Toad.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toad : MonoBehaviour {
+
+	public bool onFloor = false;
+	private Rigidbody2D rbToad;
+	private Animator animToad;
+	private int counter = 0;
+
+	void Start () {
+		rbToad = GetComponent<Rigidbody2D> ();
+		animToad = GetComponent<Animator> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (onFloor && rbToad.velocity.y == 0) {
+			if (counter == 1) {
+				rbToad.AddForce (new Vector2 (0, 10), ForceMode2D.Impulse);
+				animToad.SetBool ("ToadJump", true);
+				counter = 0;
+			} else
+				counter += 1;
+		}
+
+		if (onFloor) {
+			animToad.SetBool ("ToadJump", false);
+		} else {
+			animToad.SetBool ("ToadJump", true);
+		}
+
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "floor") {
+			onFloor = true;
+			//animToad.SetBool ("ToadJump", false);
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D coll) {
+		if (coll.gameObject.tag == "floor") {
+			onFloor = false;
+			//animToad.SetBool ("ToadJump", false);
+		}
+	}
+
+	void OnCollisionStay2D(Collision2D coll) {
+		if (coll.gameObject.tag == "floor") {
+			onFloor = true;
+			//animToad.SetBool ("ToadJump", false);
+		} else {
+			//onFloor = false;
+		}
+	}
+}
--- src/SuperMarioBros/Assets/Scripts/ToadBag.cs
+++ src/SuperMarioBros/Assets/Scripts/ToadBag.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class ToadBag : MonoBehaviour {
 
+	private Animator anim;
+	private Transform trans;
+	private bool isLock = false;
 
+	public GameObject toadPrefab;
+	public Vector3 pos;
 
 	void Start () {
-		
+		anim = GetComponent<Animator> ();
+		trans = GetComponent<Transform> ();
+
+		pos = trans.position;
 	}
 
 	void Update () {
-		
+		if (anim.GetCurrentAnimatorStateInfo (0).normalizedTime >= 0.9 && !isLock) {
+			Instantiate (toadPrefab, pos, transform.localRotation);
+			isLock = true;
+		}
 	}
 }
