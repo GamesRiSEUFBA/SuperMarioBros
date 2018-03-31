79c4f818a8e952b9af959d750acd10293645099b
Caio <dzj94116@kopqi.com>
2018-03-30 23:58:40 -0300

    Reworked the basic movement system to make it more comfortable for the player

--- src/SuperMarioBros/Assets/Scripts/MarioLuigi.cs
+++ src/SuperMarioBros/Assets/Scripts/MarioLuigi.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,6 +11,16 @@ public class MarioLuigi : MonoBehaviour {
 	private bool rightKeyPressed = false, leftKeyPressed = false, jumpPressed = false, firePressed = false, turnTime = false, onFloor = false, onAxe = false;
 	public bool dead = false;
 	private float maxVel = 6;
+	private float spd = 0;
+	private float spd_dir = 0;
+	private bool col_dir = false;
+	private float spd_max = 0.1f;
+	private float accel = 0.01f;
+	private bool dir_right = true;
+	private bool col_right = false;
+	private bool col_left = false;
+	private bool col_up = false;
+	private bool col_bottom = false;
 
 	private Vector2 oldCamPos;
 
@@ -28,10 +38,10 @@ public class MarioLuigi : MonoBehaviour {
 
 		if (Input.GetKeyUp (KeyCode.LeftArrow)) {
 			leftKeyPressed = false;
-			anim.SetBool ("Turn", false);
+			//anim.SetBool ("Turn", false);
 		} else if (Input.GetKeyUp (KeyCode.RightArrow)) {
 			rightKeyPressed = false;
-			anim.SetBool ("Turn", false);
+			//anim.SetBool ("Turn", false);
 		}
 
 		if (Input.GetKeyDown (KeyCode.C)) {
@@ -43,21 +53,16 @@ public class MarioLuigi : MonoBehaviour {
 
 		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
 			leftKeyPressed = true;
-			if (Mathf.Abs(rb.velocity.x) > 4) {
-				turnTime = true;
-				anim.SetBool ("Turn", true);
-				rb.AddForce (new Vector2 (1.2f, 0), ForceMode2D.Impulse);
-			}
 		} else if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			
 			rightKeyPressed = true;
-			if (Mathf.Abs(rb.velocity.x) > 4) {
-				turnTime = true;
-				anim.SetBool ("Turn", true);
-				rb.AddForce (new Vector2 (-1.2f, 0), ForceMode2D.Impulse);
-			}
 		}
+		/*else
+		{
+			rightKeyPressed = false;
+			leftKeyPressed = false;
+		}*/
 	}
-
 	void updateMaxSpeed() {
 		maxVel = (onFloor) ? 6 : 5;
 
@@ -81,6 +86,7 @@ public class MarioLuigi : MonoBehaviour {
 	}
 
 	void Update () {
+		
 		if (!dead) {
 			updateMaxSpeed ();
 			checkAxe ();
@@ -96,30 +102,134 @@ public class MarioLuigi : MonoBehaviour {
 				}
 			}
 
-			if (leftKeyPressed) {
-				anim.SetBool ("Walking", true);
-				if (onFloor)
-					sprite.flipX = true;
-				if (rb.velocity.x == 0)
-					turnTime = false;
-				if (rb.velocity.x > 0 && !turnTime)
-					rb.velocity = new Vector2 (-rb.velocity.x, rb.velocity.y);
-				if (Mathf.Abs (rb.velocity.x) < maxVel)
-					rb.AddForce (new Vector2 (-16, 0), ForceMode2D.Force);
-			} else if (rightKeyPressed) {
-				anim.SetBool ("Walking", true);
-				if (onFloor)
-					sprite.flipX = false;
-				if (rb.velocity.x == 0)
-					turnTime = false;
-				if (rb.velocity.x < 0 && !turnTime)
-					rb.velocity = new Vector2 (-rb.velocity.x, rb.velocity.y);
-				if (Mathf.Abs (rb.velocity.x) < maxVel)
-					rb.AddForce (new Vector2 (16, 0), ForceMode2D.Force);
+			if (leftKeyPressed)
+			{
+				if (col_left == true)
+				{
+					Debug.Log ("Can't move, there's a colllision on the left side!");
+				}
+				else
+				{
+					if (onFloor)
+						sprite.flipX = true;
+					//if (rb.velocity.x == 0)
+						//turnTime = false;
+					/*
+					if (rb.velocity.x > 0 && !turnTime)
+						rb.velocity = new Vector2 (-rb.velocity.x, rb.velocity.y);
+					if (Mathf.Abs (rb.velocity.x) < maxVel)
+						rb.AddForce (new Vector2 (-16, 0), ForceMode2D.Force);
+						*/
+
+					dir_right = false;
+					if (Mathf.Abs(spd) < spd_max)
+						spd -= accel;
+					else if (spd > 0)
+						spd -= accel * 0.5f;
+					var tempPosition = transform.position;
+					tempPosition.x = tempPosition.x + spd;
+
+					transform.position = tempPosition;
+
+					if (onFloor == true)
+					{			
+						if (spd > 0 && turnTime == false)
+						{
+							turnTime = true;
+							anim.SetBool ("Turn", true);
+							//rb.AddForce (new Vector2 (1.2f, 0), ForceMode2D.Impulse);
+						}
+						else if (spd < 0)
+						{
+							turnTime = false;
+							anim.SetBool ("Turn", false);
+							anim.SetBool ("Walking", true);
+						}
+					}
+				}
+			} 
+			else if (rightKeyPressed)
+			{
+				//anim.SetBool ("Walking", true);
+				if (col_right == true)
+				{
+					Debug.Log ("Can't move, there's a colllision on the right side!");
+				}
+				else
+				{
+					if (onFloor)
+						sprite.flipX = false;
+					//if (rb.velocity.x == 0)
+						//turnTime = false;
+					/*
+					if (rb.velocity.x < 0 && !turnTime)
+						rb.velocity = new Vector2 (-rb.velocity.x, rb.velocity.y);
+					if (Mathf.Abs (rb.velocity.x) < maxVel)
+						rb.AddForce (new Vector2 (16, 0), ForceMode2D.Force);
+					*/
+
+					dir_right = true;
+					if (spd < 0)
+						spd += accel * 0.5f;
+					else if (spd < spd_max)
+						spd += accel;
+
+					var tempPosition = transform.position;
+					tempPosition.x = tempPosition.x + spd;
+
+					transform.position = tempPosition;
+
+					if (onFloor == true)
+					{			
+						if (spd < 0 && turnTime == false)
+						{
+							turnTime = true;
+							anim.SetBool ("Turn", true);
+							//rb.AddForce (new Vector2 (1.2f, 0), ForceMode2D.Impulse);
+						}
+						else if (spd > 0) 
+						{
+							turnTime = false;
+							anim.SetBool ("Turn", false);
+							anim.SetBool ("Walking", true);
+						}
+					}
+				}
 			} else {
 				anim.SetBool ("Walking", false);
-				if (rb.velocity.x != 0)
-					rb.AddForce (new Vector2 ((rb.velocity.x / Mathf.Abs (rb.velocity.x)) * (-15), 0), ForceMode2D.Force);
+				/*if (rb.velocity.x != 0)
+					rb.AddForce (new Vector2 ((rb.velocity.x / Mathf.Abs (rb.velocity.x)) * (-15), 0), ForceMode2D.Force);*/
+				if (spd != 0)
+				{
+					if (dir_right == true)
+					{
+						col_dir = col_right;
+					} 
+					else
+					{
+						col_dir = col_left;
+					}
+
+					if (col_dir == false)
+					{
+						var tempPosition = transform.position;
+						tempPosition.x = tempPosition.x + spd;
+
+						transform.position = tempPosition;
+						
+						if (spd < 0)
+						{
+							spd += 0.005f;
+							if (spd > 0)
+								spd = 0;
+						} else if (spd > 0)
+						{
+							spd -= 0.005f;
+							if (spd < 0)
+								spd = 0;
+						}
+					}
+				}
 			}
 		}
 	}
@@ -141,10 +251,10 @@ public class MarioLuigi : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D coll) {
 		if (dead)
 			return;
-		
+		//Debug.Log ("i am the bone of my peru");
 		Vector2 collPos = coll.gameObject.transform.position;
 		Vector2 rbPos = rb.position;
-
+		Debug.Log (rbPos);
 		if (coll.gameObject.tag == "axe") {
 			onAxe = true;
 			Destroy (coll.gameObject);
@@ -163,9 +273,45 @@ public class MarioLuigi : MonoBehaviour {
 			KillMario ();
 		}
 
-		if (coll.gameObject.tag == "block1" || coll.gameObject.tag == "pipe" || coll.gameObject.tag == "block2" || coll.gameObject.tag == "blocksurp" || coll.gameObject.tag == "floor") {
-			rb.velocity = new Vector2 (rb.velocity.x, 0);
+		//if (coll.gameObject.tag == "floor" || coll.gameObject.tag == "pipe" || coll.gameObject.tag == "block2" || coll.gameObject.tag == "blocksurp" || coll.gameObject.tag == "floor") 
+		{
+			//rb.velocity = new Vector2 (rb.velocity.x, 0);
+
+			foreach (ContactPoint2D hitPos in coll.contacts)
+			{
+				Debug.Log (hitPos.normal);
+
+				if (hitPos.normal.y < 0)
+					col_bottom = true;
+				else if (hitPos.normal.y > 0)
+				{
+					//do something
+				}
+				//else
+				{
+					if (hitPos.normal.x > 0)
+						col_left = true;
+					else
+						col_left = false;
+
+					if (hitPos.normal.x < 0)
+						col_right = true;
+					else
+						col_right = false;
+
+					if (dir_right == true)
+						col_dir = col_right;
+					else
+						col_dir = col_left;
+
+					if (spd != 0 && col_dir == true)
+					{
+						spd = 0;
+					}
+				}
+			}
 		}
+			
 	}
 
 	void OnCollisionStay2D(Collision2D coll) {
@@ -174,17 +320,55 @@ public class MarioLuigi : MonoBehaviour {
 		
 		if (coll.gameObject.tag == "block1" || coll.gameObject.tag == "pipe" || coll.gameObject.tag == "block2" || coll.gameObject.tag == "blocksurp" || coll.gameObject.tag == "floor") {
 			onFloor = true;
+			col_bottom = true;
 		} else {
 			onFloor = false;
+			col_bottom = false;
 		}
+		/*
+		foreach (ContactPoint2D hitPos in coll.contacts)
+		{
+			Debug.Log (hitPos.normal);
+
+			if (hitPos.normal.y < 0)
+				col_bottom = true;
+			if (hitPos.normal.x < 0)
+				col_left = true;
+			else
+				col_left = false;
+
+			if (hitPos.normal.x > 0)
+				col_right = true;
+			else
+				col_right = false;
+		}
+		*/
 	}
 
 	void OnCollisionExit2D(Collision2D coll) {
 		if (dead)
 			return;
 		
-		if (coll.gameObject.tag == "block1" || coll.gameObject.tag == "pipe" || coll.gameObject.tag == "block2" || coll.gameObject.tag == "blocksurp" || coll.gameObject.tag == "floor") {
-			onFloor = false;
+		//if (coll.gameObject.tag == "block1" || coll.gameObject.tag == "pipe" || coll.gameObject.tag == "block2" || coll.gameObject.tag == "blocksurp" || coll.gameObject.tag == "floor") 
+		{
+			//onFloor = false;
+			//col_bottom = false;
+
+			//foreach (ContactPoint2D hitPos in coll.contacts)
+			{
+				//Debug.Log (hitPos.normal);
+				//if (hitPos.normal.x < 0)
+					col_right = false;
+				//else if (hitPos.normal.x > 0)
+					col_left = false;
+
+				//if (hitPos.normal.y > 0)
+				//{
+					col_bottom = false;
+					onFloor = false;
+				//}
+			}
 		}
 	}
 }
+
